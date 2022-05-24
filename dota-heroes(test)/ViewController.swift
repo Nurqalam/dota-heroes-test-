0ad67b833d1b221344887b5c594be741caffcc10
Nurqalam <tnurkalam@gmail.com>
2022-05-25 01:01:00 +0500

    Initial Commit

---
+++ dota-heroes(test)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dota-heroes(test)
+//
+//  Created by Nurqalam on 25.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
